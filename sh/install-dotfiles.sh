6c39804216f85214efe0b968fbd13a2f063dd158
Raihan <12584890+17radf@users.noreply.github.com>
2021-06-26 17:45:15 +0700

    sh: add vimrc hardlink

--- sh/install-dotfiles.sh
+++ sh/install-dotfiles.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.aliases.zsh ~/.aliases.zsh
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
