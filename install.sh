f4430da2fff2e29e8339f23e65ac7db93724d5bc
Raihan <12584890+17radf@users.noreply.github.com>
2021-07-01 15:52:57 +0700

    sh: remove ohmyzsh

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ sudo apt install zsh \
     zip \
     unzip 
 
-# Install zsh 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
-
 # Install antibody 
 curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
 antibody bundle < ~/dotfiles/.zsh_plugins
