73d291103be8033dd066779baca884eebfaada36
Raihan <12584890+17radf@users.noreply.github.com>
2021-07-01 15:36:05 +0700

    aliases: add listing

--- .aliases.zsh
+++ .aliases.zsh
@@ -12,4 +12,10 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias towin="cd /mnt/c/Users/raihan"
 
+# listing
+alias ls='ls --color=auto'
+alias ll='ls -la'
+alias l.='ls -d .* --color=auto'
+
+
 mkcd () { mkdir -p -- "$1" && cd -P -- "$1" }
