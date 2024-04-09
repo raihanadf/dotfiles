91ead0fd6e996813f4859b21b954608c01c943a0
Raihan <17radf@gmail.com>
2024-04-09 21:20:48 +0700

    scripts: use super instead of alt for triggering xrdb

--- scripts/wal-sync.sh
+++ scripts/wal-sync.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 1 ]
 then
 	# set wallpaper and sync color
-	wal -i "$1" && sleep 0.7 && xdotool key alt+F5
+	wal -i "$1"
 
 	# set display manager wallpaper
 	cp $1 /usr/share/sddm/themes/dimana-tema-sddm-aku/background.jpg
@@ -12,6 +12,8 @@ then
 	betterlockscreen -u "$1" --fx dim &
 	dunstify "Wallpaper Synced"
 else
-	wal -R && sleep 0.7 && xdotool key alt+F5
+	wal -R
 	dunstify "Wallpaper Restored"
 fi
+
+sleep 0.7 && xdotool key super+F5
