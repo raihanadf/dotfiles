3e872fd2b9c7e3d82a869d79e4e372021635e0dc
Raihan <17radf@gmail.com>
2024-11-02 19:43:59 +0800

    scripts: use file as state-storing

--- scripts/toggle-touchpad.sh
+++ scripts/toggle-touchpad.sh
@@ -3,7 +3,4 @@
 device="$(xinput list | grep -P '(?<= )[\w\s:]*Touchpad(?!.*Synaptics)(?i)(?-i).*?(?=\s*id)' -o | head -n1)"
 
 # enable or disable
-[[ "$(xinput list-props "$device" | grep -P ".*Device Enabled.*\K.(?=$)" -o)" == "1" ]] &&
-    xinput disable "$device" ||
-    xinput enable "$device"
-
+[ -f /tmp/touchpad_disabled ] && xinput enable "$device" && rm /tmp/touchpad_disabled || xinput disable "$device" && touch /tmp/touchpad_disabled
