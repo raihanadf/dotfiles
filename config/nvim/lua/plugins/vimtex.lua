003ea4813160fd5fe5bb58e764f0e7902caf3fc9
Raihan <17radf@gmail.com>
2025-02-13 18:49:58 +0700

    feat: update for windows

--- config/nvim/lua/plugins/vimtex.lua
+++ config/nvim/lua/plugins/vimtex.lua
@@ -2,7 +2,9 @@ return {
 	{
 		'lervag/vimtex',
 		init = function()
-			vim.g.vimtex_view_method = 'zathura'
+			vim.g.vimtex_view_method = 'sioyek'
+			vim.g.vimtex_view_sioyek_exe = 'sioyek.exe'
+			vim.g.vimtex_callback_progpath = 'wsl_nvim'
 		end
 	},
 	{
