779f4472c5d4b813f886474d9614f5c728b24655
Raihan <17radf@gmail.com>
2025-05-11 13:46:35 +0700

    nvim(lspconfig): fix setup handlers

--- config/nvim/lua/lsp/lsp.lua
+++ config/nvim/lua/lsp/lsp.lua
@@ -1,118 +1,117 @@
 -- NOTE: This is where your plugins related to LSP can be installed.
 --  The configuration is done below. Search for lspconfig to find it below.
 return {
-  -- LSP Configuration & Plugins
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', config = true },
-    'williamboman/mason-lspconfig.nvim',
+	-- LSP Configuration & Plugins
+	'neovim/nvim-lspconfig',
+	dependencies = {
+		-- Automatically install LSPs to stdpath for neovim
+		{ 'williamboman/mason.nvim', config = true },
+		'williamboman/mason-lspconfig.nvim',
 
-    -- Useful status updates for LSP
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim',       opts = {} },
+		-- Useful status updates for LSP
+		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+		{ 'j-hui/fidget.nvim',       opts = {} },
 
-    -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim',
-  },
-  config = function()
-    -- [[ Configure LSP ]]
-    --  This function gets run when an LSP connects to a particular buffer.
-    local on_attach = function(_, bufnr)
-      -- NOTE: Remember that lua is a real programming language, and as such it is possible
-      -- to define small helper and utility functions so you don't have to repeat yourself
-      -- many times.
+		-- Additional lua configuration, makes nvim stuff amazing!
+		'folke/neodev.nvim',
+	},
+	config = function()
+		-- [[ Configure LSP ]]
+		--  This function gets run when an LSP connects to a particular buffer.
+		local on_attach = function(_, bufnr)
+			-- NOTE: Remember that lua is a real programming language, and as such it is possible
+			-- to define small helper and utility functions so you don't have to repeat yourself
+			-- many times.
 
-      -- Create a command `:Format` local to the LSP buffer
-      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-      end, { desc = 'Format current buffer with LSP' })
-    end
+			-- Create a command `:Format` local to the LSP buffer
+			vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+				vim.lsp.buf.format()
+			end, { desc = 'Format current buffer with LSP' })
+		end
 
-    -- mason-lspconfig requires that these setup functions are called in this order
-    -- before setting up the servers.
-    require('mason').setup()
-    require('mason-lspconfig').setup()
+		-- mason-lspconfig requires that these setup functions are called in this order
+		-- before setting up the servers.
+		require('mason').setup()
+		require('mason-lspconfig').setup()
 
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. They will be passed to
-    --  the `settings` field of the server config. You must look up that documentation yourself.
-    --
-    --  If you want to override the default filetypes that your language server will attach to you can
-    --  define the property 'filetypes' to the map in question.
-    local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      emmet_ls = {
-        filetypes = {
-          "astro",
-          "blade",
-          "css",
-          "eruby",
-          "html",
-          "htmldjango",
-          "javascriptreact",
-          "less",
-          "pug",
-          "sass",
-          "scss",
-          "svelte",
-          "typescriptreact",
-          "vue",
-        },
-      },
-      texlab = {},
-      pyright = {},
-      -- rust_analyzer = {},
-      html = { filetypes = { 'html', 'twig', 'hbs', 'blade' } },
-      tailwindcss = { filetypes = { 'html', 'twig', 'hbs', 'blade' } },
+		-- Enable the following language servers
+		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+		--
+		--  Add any additional override configuration in the following tables. They will be passed to
+		--  the `settings` field of the server config. You must look up that documentation yourself.
+		--
+		--  If you want to override the default filetypes that your language server will attach to you can
+		--  define the property 'filetypes' to the map in question.
+		local servers = {
+			-- clangd = {},
+			-- gopls = {},
+			emmet_ls = {
+				filetypes = {
+					"astro",
+					"blade",
+					"css",
+					"eruby",
+					"html",
+					"htmldjango",
+					"javascriptreact",
+					"less",
+					"pug",
+					"sass",
+					"scss",
+					"svelte",
+					"typescriptreact",
+					"vue",
+				},
+			},
+			texlab = {},
+			pyright = {},
+			-- rust_analyzer = {},
+			html = { filetypes = { 'html', 'twig', 'hbs', 'blade' } },
+			tailwindcss = { filetypes = { 'html', 'twig', 'hbs', 'blade' } },
 
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-          -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-          -- diagnostics = { disable = { 'missing-fields' } },
-        },
-      },
+			lua_ls = {
+				Lua = {
+					workspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+					-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+					-- diagnostics = { disable = { 'missing-fields' } },
+				},
+			},
 
-      -- phpactor = {},
-    }
+			-- phpactor = {},
+		}
 
-    -- Setup neovim lua configuration
-    require('neodev').setup()
+		-- Setup neovim lua configuration
+		require('neodev').setup()
 
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-    -- Ensure the servers above are installed
-    local lspconfig = require 'lspconfig'
-    local mason_lspconfig = require 'mason-lspconfig'
+		-- Ensure the servers above are installed
+		local lspconfig = require 'lspconfig'
+		local mason_lspconfig = require 'mason-lspconfig'
 
-    mason_lspconfig.setup {
-      ensure_installed = vim.tbl_keys(servers),
-    }
+		mason_lspconfig.setup {
+			ensure_installed = vim.tbl_keys(servers),
+			handlers = {
+				function(server_name)
+					require('lspconfig')[server_name].setup {
+						capabilities = capabilities,
+						on_attach = on_attach,
+						settings = servers[server_name],
+						filetypes = (servers[server_name] or {}).filetypes,
+					}
+				end,
+			}
+		}
 
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        }
-      end,
-    }
+		lspconfig.intelephense.setup {
+			init_options = {
+				globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
+			}
+		}
 
-    lspconfig.intelephense.setup {
-      init_options = {
-        globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
-      }
-    }
-
-    vim.diagnostic.config({ virtual_text = false })
-  end
+		vim.diagnostic.config({ virtual_text = false })
+	end
 }
